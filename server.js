40579e31836150e0777112219f4a3791b0015524
khan900 <shahbazkhan185@gmail.com>
2017-02-05 20:27:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 //importing packages
 var express = require('express'); //library used to create the webserver, (how to listen to ports, handling http connections) 
-var morgan = require('morgan');
-var path = require('path');
+var morgan = require('morgan'); //to help us output log sof our server, so that we know what requests are coming to server & how we're responding
 
 var app = express();
 app.use(morgan('combined'));
 
+
+//handling specific URLs
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one', function (req,res){
+   res.send('Article one is requested and will be served here') ;
+});
+app.get('/article-two', function (req,res){
+   res.send('Article two is requested and will be served here') ;
+});
+app.get('/article-three', function (req,res){
+   res.send('Article three is requested and will be served here') ;
 });
 
 app.get('/ui/style.css', function (req, res) {
