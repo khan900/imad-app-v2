aa13366ffd59124f58f3691eaff36e662a2f50e3
khan900 <shahbazkhan185@gmail.com>
2017-02-11 14:31:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req,res){
     //articleName = article-one
-    //articles[articleName] = {} content object for article one
+    //articles[articleName] -> {} content object for article one
     //express framework provision
     articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));
