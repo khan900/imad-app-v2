d9ae03121bdf28eb3d9366e7c0076df313b0b637
khan900 <shahbazkhan185@gmail.com>
2017-02-11 21:49:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,13 @@ elem.innerHTML = "This is a new value - modified by JS";
 //move madi
 var img = document.getElementById('madi');
 
+var marginLeft = 0
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,50);
 };
