7c10617a432fba0e3e6aae9d0505b19d3043aa77
khan900 <shahbazkhan185@gmail.com>
2017-02-11 22:14:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req,res){
     //articleName = article-one
     //articles[articleName] -> {} content object for article one
@@ -110,11 +116,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-  counter = counter + 1;
-  res.send(counter.toString());
-});
 
 
 
